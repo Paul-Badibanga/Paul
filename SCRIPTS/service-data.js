747969de77fc25e92fe29fddf42a79c3d6e8914a
Paultech <paulbadibanga390@gmail.com>
2023-07-15 17:28:28 +0200

    servvices data

--- SCRIPTS/service-data.js
+++ SCRIPTS/service-data.js
@@ -6,3 +6,103 @@ const electricityData = [
     }
 ]
 console.log(electricityData)
+
+/*---------------------SERVICES SVAING DATA------------------------*/
+
+/*---------------------solar energy data------------------------*/
+const Electricity = {
+    solarEnergy : `
+    <div class="services-section">
+    <h2>Electricity</h2>
+    <div class="partition">
+    <!--------------------first partion------------------>
+        <h3>Solar Energy</h3>
+        <div class="section-grid">
+            <div class="image-section">
+                <img src="IMAGES/pannel2.jpg">
+            </div>
+            <div class="text-section">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Optio sed nisi quasi, nemo voluptatibus nesciunt quibusdam 
+                    consequuntur perferendis porro excepturi ab quidem cumque 
+                    voluptatum recusandae, saepe accusantium consectetur
+                    est molestias.</p>
+            </div>
+        </div>
+    </div>
+`,
+alternativeCurent: 
+`<div class="services-section">
+    <h2>Electricity</h2>
+    <div class="partition">
+    <!--------------------second partion------------------>
+        <h3>Low voltage & high voltage</h3>
+        <div class="section-grid">
+            <div class="image-section">
+                <img src="IMAGES/circuitBreaker.jpg">
+            </div>
+            <div class="text-section">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Optio sed nisi quasi, nemo voluptatibus nesciunt quibusdam 
+                    consequuntur perferendis porro excepturi ab quidem cumque 
+                    voluptatum recusandae, saepe accusantium consectetur
+                    est molestias.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+`
+}
+
+/*--------------------------plumbing-----------------------*/
+const plumbing = {
+    all : `
+    <h3>Industrial-Plumbing</h3>
+    <div class="section-grid">
+        <div class="image-section">
+            <img src="IMAGES/water-resources-2251633.jpg">
+        </div>
+        <div class="text-section">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Optio sed nisi quasi, nemo voluptatibus nesciunt quibusdam 
+                consequuntur perferendis porro excepturi ab quidem cumque 
+                voluptatum recusandae, saepe accusantium consectetur
+                est molestias.</p>
+        </div>
+    </div>
+    `
+}
+const welding = `
+<h2>Welding</h2>
+<div class="partition">
+<!--------------------first partion------------------>
+    <h3>Home-Construction</h3>
+    <div class="section-grid">
+        <div class="image-section">
+            <img src="IMAGES/welder2.jpg">
+        </div>
+        <div class="text-section">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Optio sed nisi quasi, nemo voluptatibus nesciunt quibusdam 
+                consequuntur perferendis porro excepturi ab quidem cumque 
+                voluptatum recusandae, saepe accusantium consectetur
+                est molestias.</p>
+        </div>
+    </div>
+<!--------------------second partion------------------>
+<h3>Material designing</h3>
+<div class="section-grid">
+    <div class="image-section">
+        <img src="IMAGES/welding.jpg">
+    </div>
+    <div class="text-section">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Optio sed nisi quasi, nemo voluptatibus nesciunt quibusdam 
+            consequuntur perferendis porro excepturi ab quidem cumque 
+            voluptatum recusandae, saepe accusantium consectetur
+            est molestias.</p>
+    </div>
+</div>
+</div>
+`
