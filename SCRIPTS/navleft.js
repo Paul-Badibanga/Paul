5f5d1dcf97002a1c23e99bb9fec6fa1dd9394bd3
Paultech <paulbadibanga390@gmail.com>
2023-07-17 13:26:00 +0200

    adding sublist toogle

--- SCRIPTS/navleft.js
+++ SCRIPTS/navleft.js
@@ -23,7 +23,24 @@ changeImage.addEventListener('click', ()=>{
 /*---------------------selection option bar--------------------------*/
 let selection = document.getElementById('choice');
 let labelEL = document.getElementsByTagName('label')
-console.log(labelEL.innerHTML);
+/*console.log(labelEL.innerHTML);*/
 selection.addEventListener('click', (e)=>{
     e.target.value
+})
+
+/*-------------------------sub-class-btn--------------------*/
+let expandIcon = document.querySelector('.expand-down-btn');
+var subList = document.getElementById('sub-list'); 
+
+expandIcon.addEventListener('click', ()=>{
+    
+    subList.classList.toggle('sub-list')
+    if(subList.classList.contains('sub-list')){
+        subList.style.display = 'block'
+        console.log('hi')
+    }
+    else {
+        subList.style.display = 'none'
+        console.log('bye')
+    }
 })
