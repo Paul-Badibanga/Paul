b192d01184cf1558764c0e131e5379d29c25f8c7
Paultech <paulbadibanga390@gmail.com>
2023-08-12 10:33:13 +0200

    storing and generating data

--- SCRIPTS/news-data.js
+++ SCRIPTS/news-data.js
@@ -1,9 +1,9 @@
 const products = [
     {
-        title : 'Phone Flashing',
-        image : 'IMAGES/smartphone-repair_1280.jpg',
-        date : '09/08/2023',
-        texContent : `
+        title: 'Phone Flashing',
+        image: 'IMAGES/smartphone-repair_1280.jpg',
+        date: '09/08/2023',
+        texContent: `
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Quasi aliquam a officia praesentium nobis libero iste sit blanditiis
         sapiente quaerat nulla doloribus ut 
@@ -12,34 +12,41 @@ const products = [
     },
 
     {
-        title : 'Phone Flashing',
-        image : 'IMAGES/smartphone-repair_1280.jpg',
-        date : '09/08/2023',
-        texContent : `
+        title: 'Artificial Intelligence',
+        image: 'IMAGES/arduino.jpg',
+        date: '14/08/2023',
+        texContent: `
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
         Quasi aliquam a officia praesentium nobis libero iste sit blanditiis
         sapiente quaerat nulla doloribus ut 
         accusantium fuga, expedita, mollitia totam deserunt quas.`
-    }
+    },
+    {
+        title: 'Camera Installation',
+        image: 'IMAGES/cctv.jpg',
+        date: '14/08/2023',
+        texContent: `
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Quasi aliquam a officia praesentium nobis libero iste sit blanditiis
+        sapiente quaerat nulla doloribus ut 
+        accusantium fuga, expedita, mollitia totam deserunt quas.`
+    },
 ]
 let dataHtml = ''
-products.forEach((product)=>{
+products.forEach((product) => {
     dataHtml = dataHtml + `
     <div class="container">
-        <div class="title">Phone Flashing</div>
+        <div class="title">${product.title}</div>
         <div class="gridContainer">
             <div class="image">
-                <img src="IMAGES/smartphone-repair_1280.jpg">
-                <div class="date">09/08/2023</div>
+                <img src="${product.image}">
+                <div class="date">${product.date}</div>
             </div>
-            <div class="detailsContents">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Quasi aliquam a officia praesentium nobis libero iste sit blanditiis
-                    sapiente quaerat nulla doloribus ut 
-                    accusantium fuga, expedita, mollitia totam deserunt quas.
-            </div>
+            <div class="detailsContents">${product.texContent}</div>
         </div>
     </div>
     `
 })
-let newsinerHtml = document.getElementsByTagName('main');
+let newsinerHtml = document.querySelector('.Maincontainer');
+newsinerHtml.innerHTML = dataHtml
+console.log(newsinerHtml.innerHTML)
