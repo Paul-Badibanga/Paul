1110eba6cdae01e18c28e8da031630fc00be0520
Paultech <paulbadibanga390@gmail.com>
2023-08-15 20:22:58 +0200

    onclick service on wide service

--- SCRIPTS/navleft.js
+++ SCRIPTS/navleft.js
@@ -90,14 +90,4 @@ services.welding.parent.addEventListener('click', ()=>{
     }
 })
 
-/*------------------------ONCLICK SERVICE BUTTON----------------------*/
-var service = document.getElementById('service-btn');
-var serviceContainer = document.querySelector('.sub-serv-js');
-service.addEventListener('click', ()=> {
-    const toggle = serviceContainer.classList.toggle('sub-serv-js')
-    if (toggle === false) {
-        serviceContainer.style.display = 'block'
-    }else {
-        serviceContainer.style.display = 'none'
-    }
-})
+
