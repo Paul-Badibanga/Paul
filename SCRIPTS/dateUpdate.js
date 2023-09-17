4bc48d5771463ac194ee708986ce311d94407b2b
Paultech <paulbadibanga390@gmail.com>
2023-09-17 11:26:40 +0200

    creating file for copyeright date updating

---
+++ SCRIPTS/dateUpdate.js
@@ -0,0 +1,5 @@
+/*-------------------DATE FOR COPY RIGHT---------------------*/
+const date = new Date();
+let year = date.getFullYear()
+console.log(year)
+document.querySelector('.date').innerHTML = year;
